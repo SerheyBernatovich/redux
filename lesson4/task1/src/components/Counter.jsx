7ec63f9f954e4038a144acaa2e5566c4392d1bee
SerheyBernatovich <sec1982@ukr.net>
2022-01-31 12:42:21 +0200

    add

--- lesson4/task1/src/components/Counter.jsx
+++ lesson4/task1/src/components/Counter.jsx
@@ -5,13 +5,13 @@ import { increment, decrement, reset } from '../counter.actions';
 const Counter = ({ counter, incr, decr, res }) => {
   return (
     <div className="counter">
-      <button className="counter__button" onClick={() => decr()}>
+      <button className="counter__button" onClick={decr}>
         -
       </button>
-      <span className="counter__value" onClick={() => res()}>
+      <span className="counter__value" onClick={res}>
         {counter}
       </span>
-      <button className="counter__button" onClick={() => incr()}>
+      <button className="counter__button" onClick={incr}>
         +
       </button>
     </div>
